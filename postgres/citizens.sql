497433b0de84a99547b4f4d6a5cf93a57874d16c
Sara Tomillo <saragleztomillo@gmail.com>
2017-03-29 10:44:23 +0200

    script db

---
+++ postgres/citizens.sql
@@ -0,0 +1,85 @@
+﻿-- Database: citizens
+
+-- DROP DATABASE citizens;
+
+CREATE DATABASE citizens
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Spain.1252'
+       LC_CTYPE = 'Spanish_Spain.1252'
+       CONNECTION LIMIT = -1;
+
+- Table: public.commentary
+
+-- DROP TABLE public.commentary;
+
+CREATE TABLE public.commentary
+(
+  id integer NOT NULL DEFAULT nextval('commentary_id_seq'::regclass),
+  content text,
+  votes integer,
+  fecha date,
+  user_id integer,
+  proposal_id integer,
+  CONSTRAINT commentary_pkey PRIMARY KEY (id),
+  CONSTRAINT commentary_proposal_id_fkey FOREIGN KEY (proposal_id)
+      REFERENCES public.proposal (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT commentary_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES public."user" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.commentary
+  OWNER TO postgres;
+
+  
+- Table: public."user"
+
+-- DROP TABLE public."user";
+
+CREATE TABLE public."user"
+(
+  id integer NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+  dni text NOT NULL,
+  nombre text,
+  apeliidos text,
+  email text,
+  nacimiento date,
+  direccion text,
+  nacinalidad text,
+  polling integer,
+  CONSTRAINT user_pkey PRIMARY KEY (dni),
+  CONSTRAINT user_id_key UNIQUE (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."user"
+  OWNER TO postgres;
+
+  
+-- Table: public.proposal
+
+-- DROP TABLE public.proposal;
+
+CREATE TABLE public.proposal
+(
+  id integer NOT NULL DEFAULT nextval('proposal_id_seq'::regclass),
+  content text,
+  votes integer,
+  category text,
+  user_id integer,
+  CONSTRAINT proposal_pkey PRIMARY KEY (id),
+  CONSTRAINT proposal_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES public."user" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.proposal
+  OWNER TO postgres;
